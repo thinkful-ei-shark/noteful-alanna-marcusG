93470bb99025d3954d348afbc3fe3224ef8b7768
Alanna Bouloy <lanihope@gmail.com>
2020-10-07 15:46:50 -0400

    deleted import statement for dummy store

--- src/App/App.js
+++ src/App/App.js
@@ -5,7 +5,6 @@ import NoteListNav from '../NoteListNav/NoteListNav';
 import NotePageNav from '../NotePageNav/NotePageNav';
 import NoteListMain from '../NoteListMain/NoteListMain';
 import NotePageMain from '../NotePageMain/NotePageMain';
-import dummyStore from '../dummy-store';
 import './App.css';
 import Context from '../Context';
 import api_endpoint from '../api_endpoint';
